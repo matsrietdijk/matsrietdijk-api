e939d56d91e3d096de07bd0deec68f5260a82833
Mats Rietdijk <matsrietdijk@gmail.com>
2015-09-19 15:09:12 +0200

    Test response status 200 for api#root

--- spec/controllers/api_controller_spec.rb
+++ spec/controllers/api_controller_spec.rb
@@ -2,6 +2,11 @@ require 'rails_helper'
 
 RSpec.describe ApiController, type: :controller do
   describe 'GET #root' do
+    it 'responds successfully with status 200' do
+      get :root
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
 
   end
 end
