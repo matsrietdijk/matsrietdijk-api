9c8ad6aa5c6ab19540896924e6cbf6a67957c20c
Mats Rietdijk <matsrietdijk@gmail.com>
2015-09-25 23:26:31 +0200

    Add aasm states and transitions to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ActiveRecord::Base
+  include AASM
+
+  aasm column: :state do
+    state :concept, initial: true
+    state :published
+
+    event :publish do
+      transitions from: :concept, to: :published
+    end
+
+    event :unpublish do
+      transitions from: :published, to: :concept
+    end
+  end
 end
