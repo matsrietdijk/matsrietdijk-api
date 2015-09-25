3c533c3656c01cf35c09561eb9687b3af7062e58
Mats Rietdijk <matsrietdijk@gmail.com>
2015-09-25 23:25:35 +0200

    Add state field to posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150925151025) do
+ActiveRecord::Schema.define(version: 20150925210944) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20150925151025) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text   "body"
+    t.string "state"
   end
 
 end
