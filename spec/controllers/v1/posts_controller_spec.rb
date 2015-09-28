f3b23e673c33ea49d691de704279375bed28b55e
Mats Rietdijk <matsrietdijk@gmail.com>
2015-09-28 22:28:00 +0200

    Fix v1/posts#index body spec by adding meta

--- spec/controllers/v1/posts_controller_spec.rb
+++ spec/controllers/v1/posts_controller_spec.rb
@@ -10,7 +10,8 @@ RSpec.describe V1::PostsController, type: :controller do
 
     it 'responds with published posts json' do
       published_posts = create_list(:post, 2, :published)
-      serializable = ActiveModel::SerializableResource.new(published_posts)
+      meta = Post.page.index_meta
+      serializable = ActiveModel::SerializableResource.new(published_posts, meta: meta)
       get :index
       expect(response.body).to eq(serializable.to_json)
     end
