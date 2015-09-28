44ce7a9e3435c876911e53ad6839082911aef05e
Mats Rietdijk <matsrietdijk@gmail.com>
2015-09-28 21:22:53 +0200

    Add count to pagination on v1/posts#index

--- app/controllers/v1/posts_controller.rb
+++ app/controllers/v1/posts_controller.rb
@@ -1,7 +1,7 @@
 module V1
   class PostsController < ::V1::ApplicationController
     def index
-      @posts = Post.published.page(params[:page])
+      @posts = Post.published.page(params[:page]).per(params[:count])
       render json: @posts
     end
   end
