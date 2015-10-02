6611115ba839258ee09b0bf3dc6a2f372dfe672d
Mats Rietdijk <matsrietdijk@gmail.com>
2015-10-02 15:12:00 +0200

    Make airbrake env vars optional with standard defaults

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,6 +1,6 @@
 Airbrake.configure do |config|
   config.api_key = ENV['AIRBRAKE_API_KEY']
-  config.host = ENV['AIRBRAKE_HOST']
-  config.port = 443
+  config.host = ENV['AIRBRAKE_HOST'] if ENV['AIRBRAKE_HOST']
+  config.port = ENV['AIRBRAKE_PORT'] if ENV['AIRBRAKE_PORT']
   config.secure = config.port == 443
 end
