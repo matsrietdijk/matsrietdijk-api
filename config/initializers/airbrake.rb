32a0831ab184bc84c26dbd078435172115dea5a6
Mats Rietdijk <matsrietdijk@gmail.com>
2015-10-02 15:27:47 +0200

    Clean airbrake initializer

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,6 +1,10 @@
 Airbrake.configure do |config|
-  config.api_key = ENV['AIRBRAKE_API_KEY']
-  config.host = ENV['AIRBRAKE_HOST'] if ENV['AIRBRAKE_HOST']
-  config.port = ENV['AIRBRAKE_PORT'] if ENV['AIRBRAKE_PORT']
+  api_key = ENV['AIRBRAKE_API_KEY']
+  host = ENV['AIRBRAKE_HOST']
+  port = ENV['AIRBRAKE_PORT'].to_i
+
+  config.api_key = api_key
+  config.host = host if host
+  config.port = port if port > 0
   config.secure = config.port == 443
 end
