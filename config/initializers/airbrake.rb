fbcaddb88573f4c820f9d1834fbf65d1f0c63add
Mats Rietdijk <matsrietdijk@gmail.com>
2015-10-03 12:49:49 +0200

    Configure airbrake to use sidekiq if env var set

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -2,9 +2,15 @@ Airbrake.configure do |config|
   api_key = ENV['AIRBRAKE_API_KEY']
   host = ENV['AIRBRAKE_HOST']
   port = ENV['AIRBRAKE_PORT'].to_i
+  async = ENV['AIRBRAKE_SIDEKIQ'] == 'true'
 
   config.api_key = api_key
   config.host = host if host
   config.port = port if port > 0
   config.secure = config.port == 443
+  if async
+    config.async do |notice|
+      AirbrakeDeliveryWorker.perform_async(notice.to_xml)
+    end
+  end
 end
