b6e09d361a8ed9b2eb44821678e6691d7d432ec3
Mats Rietdijk <matsrietdijk@gmail.com>
2015-10-03 14:02:55 +0200

    Allow more airbrake configuration with env vars

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -2,15 +2,18 @@ Airbrake.configure do |config|
   api_key = ENV['AIRBRAKE_API_KEY']
   host = ENV['AIRBRAKE_HOST']
   port = ENV['AIRBRAKE_PORT'].to_i
+  secure = ENV['AIRBRAKE_SECURE'] == 'true'
   async = ENV['AIRBRAKE_SIDEKIQ'] == 'true'
+  dev = ENV['AIRBRAKE_DEV'] == 'true'
 
   config.api_key = api_key
   config.host = host if host
   config.port = port if port > 0
-  config.secure = config.port == 443
+  config.secure = secure || config.port == 443
   if async
     config.async do |notice|
       AirbrakeDeliveryWorker.perform_async(notice.to_xml)
     end
   end
+  config.development_environments.delete('development') if dev
 end
