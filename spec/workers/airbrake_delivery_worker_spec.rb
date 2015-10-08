31807880c03ca37a672439a9f801687bb53f0ed4
Mats Rietdijk <matsrietdijk@gmail.com>
2015-10-08 10:20:31 +0200

    Add basic spec for airbrake delivery worker

---
+++ spec/workers/airbrake_delivery_worker_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe AirbrakeDeliveryWorker, type: :worker do
+  it { is_expected.to be_processed_in :default }
+  it { is_expected.to be_retryable true }
+
+  it 'passes notice to airbrake' do
+    notice = Faker::Lorem.sentence
+    allow(Airbrake.sender).to receive(:send_to_airbrake)
+    subject.perform(notice)
+    expect(Airbrake.sender).to have_received(:send_to_airbrake).with(notice)
+  end
+end
