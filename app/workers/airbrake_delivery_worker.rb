611208661903c7ee30fb5207995863664c0b1590
Mats Rietdijk <matsrietdijk@gmail.com>
2015-10-09 17:03:39 +0200

    Set airbrake worker retry to 5

--- app/workers/airbrake_delivery_worker.rb
+++ app/workers/airbrake_delivery_worker.rb
@@ -2,6 +2,8 @@ class AirbrakeDeliveryWorker
   include Sidekiq::Worker
   include Airbrake
 
+  sidekiq_options retry: 5
+
   def perform(notice)
     Airbrake.sender.send_to_airbrake(notice)
   end
