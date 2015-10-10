ade48ff5cd9f34f98ef8ea9e3264498af7bcfd10
Mats Rietdijk <matsrietdijk@gmail.com>
2015-10-10 20:00:59 +0200

    Generate api token before create user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,17 @@
 class User < ActiveRecord::Base
   has_secure_password
 
+  before_create :generate_api_token
+
   validates :username, presence: true, uniqueness: true
   validates :api_token, allow_nil: true, uniqueness: true
+
+  private
+
+  def generate_api_token
+    loop do
+      self.api_token = SecureRandom.hex
+      break unless self.class.exists?(api_token: api_token)
+    end
+  end
 end
