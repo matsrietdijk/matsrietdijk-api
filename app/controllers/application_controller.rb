5c32e3d5c9b43dcd0cded93958d41a359bcaac83
Mats Rietdijk <matsrietdijk@gmail.com>
2015-10-10 13:57:17 +0200

    Add before action to authenticate user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::API
   include ActionController::HttpAuthentication::Token::ControllerMethods
+
+  before_action :authenticate_user
+
+  private
+
+  def authenticate_user
+  end
 end
