bb7063bd4461cc0179110f9ee5a537b186978132
Mats Rietdijk <matsrietdijk@gmail.com>
2015-10-11 17:09:52 +0200

    Add basic validation specs for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  PRESENCE = %w(password username).freeze
+  UNIQUE = %w(username api_token).freeze
+
+  it { is_expected.to have_secure_password }
+
+  PRESENCE.each do |field|
+    it { is_expected.to validate_presence_of(field) }
+  end
+
+  UNIQUE.each do |field|
+    it { is_expected.to validate_uniqueness_of(field) }
+  end
+
+  it { is_expected.to allow_value(nil).for(:api_token) }
 end
