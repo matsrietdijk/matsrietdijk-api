886d957743e0ce065a58a720072ef875c6ad78f9
Mats Rietdijk <matsrietdijk@gmail.com>
2015-10-12 20:08:59 +0200

    Add length to api token for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,18 @@
 class User < ActiveRecord::Base
+  API_TOKEN_LENGTH = 32
+
   has_secure_password
 
   before_create :generate_api_token
 
   validates :username, presence: true, uniqueness: true
-  validates :api_token, allow_nil: true, uniqueness: true
+  validates :api_token, allow_nil: true, uniqueness: true, length: { is: API_TOKEN_LENGTH }
 
   private
 
   def generate_api_token
     loop do
-      self.api_token = SecureRandom.hex
+      self.api_token = SecureRandom.hex(API_TOKEN_LENGTH)
       break unless self.class.exists?(api_token: api_token)
     end
   end
