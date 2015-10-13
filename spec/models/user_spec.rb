e46afc87e98d361f3dabcc01e83ceec16a6324bb
Mats Rietdijk <matsrietdijk@gmail.com>
2015-10-13 22:50:41 +0200

    Add specs for user#username format validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,6 +14,8 @@ RSpec.describe User, type: :model do
     it { is_expected.to validate_uniqueness_of(field) }
   end
 
+  it { is_expected.to allow_value('a', '0', 'a0', 'a_z').for(:username) }
+  it { is_expected.to_not allow_value('A0', 'a-z', '#', '&', '_a', 'a_').for(:username) }
   it { is_expected.to allow_value(nil).for(:api_token) }
   it { is_expected.to validate_length_of(:api_token).is_equal_to(User::API_TOKEN_LENGTH) }
 
