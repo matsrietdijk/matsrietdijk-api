2f5a1cc2fd4624085c71c94c3d09cf2109af448e
Mats Rietdijk <matsrietdijk@gmail.com>
2015-10-16 18:29:12 +0200

    Add scope for authenticated user to posts controller

--- app/controllers/v1/posts_controller.rb
+++ app/controllers/v1/posts_controller.rb
@@ -1,8 +1,14 @@
 module V1
   class PostsController < ::V1::ApplicationController
     def index
-      @posts = Post.published.page(params[:page]).per(params[:count])
+      @posts = posts_scope.page(params[:page]).per(params[:count])
       render json: @posts, meta: @posts.index_meta
+    end
+
+    private
+
+    def posts_scope
+      @current_user.present? ? Post.all : Post.published
     end
   end
 end
