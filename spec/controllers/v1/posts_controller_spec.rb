b48ea5ddad43c6bde4de02a25aaeea216ca494fa
Mats Rietdijk <matsrietdijk@gmail.com>
2015-10-16 18:22:43 +0200

    Change posts controller specs for authenticated user

--- spec/controllers/v1/posts_controller_spec.rb
+++ spec/controllers/v1/posts_controller_spec.rb
@@ -36,19 +36,21 @@ RSpec.describe V1::PostsController, type: :controller do
         expect(response).to have_http_status(200)
       end
 
-      it 'responds with published posts json' do
-        published_posts = create_list(:post, 2, :published)
-        meta = Post.published.page.index_meta
-        serializable = ActiveModel::SerializableResource.new(published_posts, meta: meta)
+      it 'responds with all posts json' do
+        create_list(:post, 2, :published)
+        create_list(:post, 2, :concept)
+        all_posts = Post.page
+        meta = all_posts.index_meta
+        serializable = ActiveModel::SerializableResource.new(all_posts, meta: meta)
         get :index
         expect(response.body).to eq(serializable.to_json)
       end
 
-      it 'assigns published posts to @posts' do
-        published_posts = create_list(:post, 2, :published)
-        create(:post, :concept)
+      it 'assigns all posts to @posts' do
+        create_list(:post, 2, :published)
+        create_list(:post, 2, :concept)
         get :index
-        expect(assigns(:posts)).to eq(published_posts)
+        expect(assigns(:posts)).to eq(Post.all)
       end
     end
   end
