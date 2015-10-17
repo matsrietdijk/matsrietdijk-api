53a37721c8415ff2a8a18eed325a32c8e2e7d349
Mats Rietdijk <matsrietdijk@gmail.com>
2015-10-17 18:46:47 +0200

    Use different post serializer based on user

--- app/controllers/v1/posts_controller.rb
+++ app/controllers/v1/posts_controller.rb
@@ -2,13 +2,17 @@ module V1
   class PostsController < ::V1::ApplicationController
     def index
       @posts = posts_scope.page(params[:page]).per(params[:count])
-      render json: @posts, meta: @posts.index_meta
+      render json: @posts, meta: @posts.index_meta, each_serializer: post_serializer
     end
 
     private
 
     def posts_scope
       @current_user.present? ? Post.all : Post.published
+    end
+
+    def post_serializer
+      @current_user.present? ? Admin::PostSerializer : Public::PostSerializer
     end
   end
 end
